b2bc5daf60f8ac6075103325db5b220981153760
hejin <hejin82@gmail.com>
2018-06-06 19:20:19 +0900

    commit

--- Beginning-Android-Programming-with-AndroidStudio-4th/UsingIntent/app/src/main/java/org/hejin/usingintent/SecondActivity.java
+++ Beginning-Android-Programming-with-AndroidStudio-4th/UsingIntent/app/src/main/java/org/hejin/usingintent/SecondActivity.java
@@ -1,7 +1,11 @@
 package org.hejin.usingintent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void onClick(View view) {
+        Intent data = new Intent();
+        EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
+        data.setData(Uri.parse(txtUsername.getText().toString()));
+        setResult(RESULT_OK, data);
+        finish();
     }
 }
