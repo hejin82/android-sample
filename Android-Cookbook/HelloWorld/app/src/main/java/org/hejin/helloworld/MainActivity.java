821a5e2d36edc5023626301ef8fb78bf10b231c5
hejin <hejin82@gmail.com>
2019-03-23 20:56:27 +0900

    commit

--- Android-Cookbook/HelloWorld/app/src/main/java/org/hejin/helloworld/MainActivity.java
+++ Android-Cookbook/HelloWorld/app/src/main/java/org/hejin/helloworld/MainActivity.java
@@ -1,13 +1,83 @@
 package org.hejin.helloworld;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "HelloWorld";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart()");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy()");
+        super.onDestroy();
+    }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        Log.d(TAG, "onBackPressed()");
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPostResume() {
+        Log.d(TAG, "onPostResume()");
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onPostCreate()");
+        super.onPostCreate(savedInstanceState);
+    }
+
+
+
+
+
+
+
+
 }
